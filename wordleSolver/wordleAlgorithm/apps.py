7c350d7b4848c01fdc7ae03732d7ceb87ad548ce
bugalpha_ <bugalpha_@localhost.localdomain>
2022-02-04 05:10:03 -0500

    1st

---
+++ wordleSolver/wordleAlgorithm/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WordlealgorithmConfig(AppConfig):
+    name = 'wordleAlgorithm'
---
+++ wordleSolver/wordleAlgorithm/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+from selenium import webdriver
+import json
+import time
+
+# wd = webdriver.Chrome()
+# wd.get("https://www.powerlanguage.co.uk/wordle/")
+# localStorage=wd.execute_script("return window.localStorage;")
+# x= json.loads(localStorage['gameState'])
+# print(x['solution'])
+
+def show(request):
+    wd = webdriver.Chrome()
+    wd.get("https://www.powerlanguage.co.uk/wordle/")
+    localStorage=wd.execute_script("return window.localStorage;")
+    wd.quit()
+    main_dict= json.loads(localStorage['gameState'])
+    x=main_dict['solution']
+    return render(request,'result.html',{'ans':x})
+    
