45c9c23d7a1edc516478342d1652fe973b9df832
hasan_ibn_amir_ <64352182+HasanIbnAmir@users.noreply.github.com>
2022-10-17 00:05:34 +0600

    Update views.py

--- wordleSolver/wordleAlgorithm/views.py
+++ wordleSolver/wordleAlgorithm/views.py
@@ -11,7 +11,7 @@ def show(request):
     crm_options.add_argument("--disable-dev-shm-usage")
     crm_options.add_argument("--no-sandbox")
     wd = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=crm_options)
-    wd.get("https://www.powerlanguage.co.uk/wordle/")
+    wd.get("https://www.nytimes.com/games/wordle/index.html")
     localStorage=wd.execute_script("return window.localStorage;")
     wd.quit()
     main_dict= json.loads(localStorage['nyt-wordle-state'])
